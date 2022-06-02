60eeeb28a9ae79e30b9c00080047eedb86b71ce3
rohitf116 <rrsonawne090909@gmail.com>
2022-06-02 18:58:50 +0530

    assignment

--- src/routes/route.js
+++ src/routes/route.js
@@ -3,6 +3,61 @@ const myHelper = require('../util/helper')
 const underscore = require('underscore')
 
 const router = express.Router();
+    //--------------------first Assignment-------------------------------//
+router.get('/movies', function (req, res) {
+
+    const movie=['Rang de basanti', 'The shining', 'Lord of the rings', 'Batman begins']
+    
+    res.send(movie)
+});
+    //--------------------second and three Assignment-------------------------------//
+router.get('/movies/:indexNumber', function (req, res) {
+
+    const movie=['Rang de basanti', 'The shining', 'Lord of the rings', 'Batman begins']
+    let ind=(Number(req.params.indexNumber)-1)
+    req.params.indexNumber >movie.length?res.send('use a valid index'): res.send(movie[ind] )
+   
+});
+
+
+    //--------------------fourth Assignment-------------------------------//
+    router.get('/films', function (req, res) {
+
+        const film=[ {
+            id:1,name:'The Shinning'
+           },  {
+            id:2,name:'Incendies'
+           }, {
+            id:3,name:'Rang de Basanti'
+           }, {
+            id:4,name:'Finding Nemo'
+           }]
+           
+        res.send(film )
+       
+    });
+
+    router.get('/films/:filmId', function (req, res) {
+
+        const film=[ {
+            id:1,name:'The Shinning'
+           },  {
+            id:2,name:'Incendies'
+           }, {
+            id:3,name:'Rang de Basanti'
+           }, {
+            id:4,name:'Finding Nemo'
+           }]
+           console.log(req.params.filmId)
+        const num=req.params.filmId
+        
+
+        res.send(film[num].name)
+       
+    });
+
+
+
 
 router.get('/test-me', function (req, res) {
     myHelper.printDate()
