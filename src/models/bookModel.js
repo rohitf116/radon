3bae59dc54614470aaa11d2fe01686389c9be5c2
rohitf116 <rrsonawne090909@gmail.com>
2022-06-07 09:39:55 +0530

    assignment mongaDB 2

--- src/models/bookModel.js
+++ src/models/bookModel.js
@@ -1,20 +1,22 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-const bookSchema = new mongoose.Schema( {
-    bookName: String, 
-    authorName: String, 
-    tags: [String],
-    
-    isPublished: Boolean,
+const bookSchema = new mongoose.Schema(
+  {
+    bookName: { type: String, required: true },
     prices: {
-        indianPrice: String,
-        europePrice: String,
+      indianPrice: String,
+      europePrice: String,
     },
-    sales: {type: Number, default: 10}
-}, { timestamps: true });
+    year: { type: Number, default: 2021 },
+    tags: [String],
+    authorName: String,
+    totalPages: Number,
+    stockAvailable: Boolean,
+  },
+  { timestamps: true }
+);
 
-
-module.exports = mongoose.model('Book', bookSchema) //users
+module.exports = mongoose.model("Book", bookSchema); //users
 
 //Validation:
 //require:true
