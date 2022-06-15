e64e92bb5a9ed659f7761f3b191ca536356d9802
rohitf116 <rrsonawne090909@gmail.com>
2022-06-15 14:38:52 +0530

    its done

--- src/auth/auth.js
+++ src/auth/auth.js
@@ -1,24 +1,29 @@
 const jwt = require('jsonwebtoken');
 const userModel = require('../models/userModel');
 exports.checkToken = async (req, res, next) => {
-  const token = req.headers['x-Auth-token'] || req.headers['x-auth-token'];
-  if (token) {
-    let token = req.headers['x-auth-token'];
-    console.log('checkToken is working');
-    const decoded = jwt.verify(token, 'functionup-radon');
-    console.log(decoded);
-    next();
+  console.log('------------------------------');
+  let token = req.headers['x-auth-token'];
+
+  if (req.headers['x-auth-token'] == undefined) {
+    res.send({ status: false, msg: 'token must be present' });
   }
 
-  //If no token is present in the request header return error
-  if (!token) return res.send({ status: false, msg: 'token must be present' });
+  if (token) {
+    const decoded = jwt.verify(token, 'functionup-radon');
+    const userId = req.params.userId;
+    const decodedid = decoded.userId;
+
+    userId != decodedid
+      ? res.send({ status: false, msg: 'token is defferent' })
+      : next();
+  }
 };
+
+//If no token is present in the request header return error
 
 exports.checkUser = async (req, res, next) => {
   let userId = req.params.userId;
-  console.log('checkUsr Working');
   let user = await userModel.findById(userId);
-  //Return an error if no user with the given id exists in the db
   if (!user) {
     return res.send('No such user exists');
   }
